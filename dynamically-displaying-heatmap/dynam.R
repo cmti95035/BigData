3cfb3968c5a550f558672404d6f0cc5f275f607e
cmti95035/BigData <star0.lin@gmail.com>
2015-05-20 16:21:20 -0700

    动态显示热图

---
+++ dynamically-displaying-heatmap/dynam.R
@@ -0,0 +1,16 @@
+#!/bin/Rscript
+args <- commandArgs(TRUE)
+CorrAns = args[1]
+library(ggmap)
+library(lattice)
+library(animation)
+library(XML)
+localmap = get_map(location = c(lon = -105.265, lat = 40.005), zoom = 15, maptype = 'hybrid', source = "google")
+localmap = ggmap(localmap, extent = 'device')
+df <- read.csv(CorrAns)
+dynam <- function(x) {
+  df$LTE_UE_Tx_Power <- df[,x]
+  p <- localmap + geom_point(aes(x = -W, y = N, colour = LTE_UE_Tx_Power), size = 7, alpha = 0.5, data = df)  + scale_colour_gradient(limits=c(-200, 50), low="green", high="red") + labs(title='LTE Measurements at University of Colorado, Boulder\n')
+}
+t <- seq(5,104)
+saveGIF(for (i in t) print(dynam(i)))
