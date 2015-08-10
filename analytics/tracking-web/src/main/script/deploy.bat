057445109e96a5263d2f6bd0167ba70fb088288d
Guobiao Mo <moguobiao@yahoo.com>
2015-08-10 00:55:01 -0700

    improve web war deployment

--- analytics/tracking-web/src/main/script/deploy.bat
+++ analytics/tracking-web/src/main/script/deploy.bat
@@ -2,8 +2,6 @@ echo on
 set CATALINA_HOME=C:\app\apache-tomcat-8.0.18
 rem set CATALINA_HOME=C:\app\apache-tomcat-7.0.42
 
-set TOMCAT_HOME=%CATALINA_HOME%
-
 call %CATALINA_HOME%\bin\shutdown.bat
 
 cd C:\git\cmti2\cmti\analytics
@@ -17,20 +15,3 @@ copy /Y C:\git\cmti2\cmti\analytics\tracking-web\target\map.war %CATALINA_HOME%\
 call %CATALINA_HOME%\bin\startup.bat
 
 cd C:\git\cmti2\cmti\analytics\tracking-web\src\main\script
-
-rem call sleep 30
-
-pause
-
-rem del /s /q  %CATALINA_HOME%\webapps\map\WEB-INF\lib\jsp-*
-rem del /s /q  %CATALINA_HOME%\webapps\map\WEB-INF\lib\jasper-*
-
-rem javax.servlet and org.apache.jasper are manually removed from %CATALINA_HOME%\webapps\map-manual\WEB-INF\lib\tracking-app-1.0-SNAPSHOT.jar
-copy /y %CATALINA_HOME%\webapps\map-manual\WEB-INF\lib\tracking-app-1.0-SNAPSHOT.jar  %CATALINA_HOME%\webapps\map\WEB-INF\lib\
- 
- 
-call %CATALINA_HOME%\bin\shutdown.bat
-
-call %CATALINA_HOME%\bin\startup.bat
-
-cd C:\git\cmti2\cmti\analytics\tracking-web\src\main\script\
